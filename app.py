80879d5d311be86a47e2b61997d1e19994e505bb
Joe Meyer <joseph@meyer.id>
2024-01-14 01:28:16 +0000

    uuid and xl model

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import StreamingResponse
-from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+from diffusers import (
+    StableDiffusionPipeline,
+    DPMSolverMultistepScheduler,
+    StableDiffusionXLPipeline,
+)
 import torch
-from threading import Lock
 from io import BytesIO
 import time
 import logging
 import asyncio
 from pydantic import BaseModel
+import uuid
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -33,7 +37,7 @@ async def log_duration(request: Request, call_next):
 MODEL_NAME = "stabilityai/stable-diffusion-2-1"
 # "runwayml/stable-diffusion-v1-5"
 # "stabilityai/stable-diffusion-2-1"
-# "stabilityai/stable-diffusion-xl-base-1.0"
+XL_MODEL_NAME = "stabilityai/stable-diffusion-xl-base-1.0"
 
 
 # Assuming you have a function load_model that loads your model
@@ -47,12 +51,23 @@ def load_model():
     return pipe
 
 
+def load_xl_model():
+    pipe = StableDiffusionXLPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-xl-base-1.0",
+        torch_dtype=torch.float16,
+        use_safetensors=True,
+        variant="fp16",
+    )
+    pipe.unet = torch.compile(pipe.unet, mode="reduce-overhead", fullgraph=True)
+    return pipe
+
+
 # Ensure model is on GPU if available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load your model and assign it to available GPUs
 num_gpus = torch.cuda.device_count()
-models = [load_model().to(f"cuda:{i}") for i in range(num_gpus)]
+models = [load_xl_model().to(f"cuda:{i}") for i in range(num_gpus)]
 
 # Locks for managing concurrent access to GPUs
 gpu_locks = [asyncio.Lock() for _ in range(num_gpus)]
@@ -66,6 +81,7 @@ logger.info(f"{num_gpus} GPU(s) initialized")
 # Define a Pydantic model for the request body
 class GenerateImageInput(BaseModel):
     prompt: str
+    negPrompt: str
 
 
 @app.post("/generate")
@@ -86,15 +102,20 @@ async def generate_image(input_data: GenerateImageInput):
                 # Ensure to use the selected GPU for computations
                 pipe = models[gpu_id]
 
-                image = pipe(prompt=input_data.prompt).images[0]
+                image = pipe(
+                    prompt=input_data.prompt, negative_prompt=input_data.negPrompt
+                ).images[0]
 
             # Convert image to bytes
             img_byte_arr = BytesIO()
             image.save(img_byte_arr, format="PNG")
             img_byte_arr.seek(0)
 
+            # Generate a random UUID
+            file_uuid = uuid.uuid4()
+
             # Define a filename
-            filename = f"image.png"
+            filename = f"{file_uuid}.png"
 
             # Return image as response with Content-Disposition header
             headers = {"Content-Disposition": f"attachment; filename={filename}"}
